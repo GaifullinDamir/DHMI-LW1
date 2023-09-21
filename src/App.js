896bffe7c99ae26dfa4871b301c52f1a0bb38472
Damir <gaifullindr@gmail.com>
2023-09-22 01:52:05 +0300

    Переделываю роутинги

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import {Routes, Route, Navigate} from 'react-router-dom';
-import AppRouter from "./components/AppRouter";
+import Greeting from "./pages/greeting/Greeting";
+import HyphothesisOne from "./pages/hypothesisOne/HypothesisOne";
+import HyphothesisTwo from "./pages/hypothesisTwo/HypothesisTwo";
+import Results from "./pages/results/Results";
+import { GREETING_ROUTE, HYPHOTHESIS_ONE_ROUTE, HYPHOTHESIS_TWO_ROUTE, RESULTS_ROUTE } from "./utils/consts";
+
 
 function App() {
+  const {username, setUsername} = useState('');
+  const {groupNumber, setGroupNumber} = useState('');
+  const {resultFirst, setResultFirst} = useState('');
+  const {resultSecond, setResultSecond} = useState('');
+
   return (
     <BrowserRouter>
-      <AppRouter/>
+      <Routes>
+        <Route path={GREETING_ROUTE} element={<Greeting setUsername={setUsername} setGroupNumber={setGroupNumber}/>} />
+        <Route path={HYPHOTHESIS_ONE_ROUTE} element={<HyphothesisOne setResultFirst={setResultFirst}/>} />
+        <Route path={HYPHOTHESIS_TWO_ROUTE} element={<HyphothesisTwo setResultSecond={setResultSecond}/>} />
+        <Route path={RESULTS_ROUTE} element={<Results data={{username, groupNumber, resultFirst, resultSecond}}/>} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/greeting/Greeting.js
+++ src/pages/greeting/Greeting.js
@@ -1,9 +1,20 @@
 import React, {useState} from 'react';
 
 const Greeting = () => {
+
+    const {username, setUsername} = useState('');
+    const {groupNumber, setGroupNumber} = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
     return(
         <div>
-            Greeting
+            <form className='greeting-form' onSubmit={handleSubmit}>
+                <input required placeholder="Ваше имя" type="text" onChange={e => setUsername(e.target.value)}/>
+                <input required placeholder="Ваше группа" type="text" onChange={e => setGroupNumber(e.target.value)}/>
+                <button class="button button_submit">Начать тестирование</button>
+            </form>
         </div>
     )
 };
