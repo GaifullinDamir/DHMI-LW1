140e0e33ce3ae440afeab7025d620be4fc07f0e4
Damir <gaifullindr@gmail.com>
2023-09-22 09:16:15 +0300

    Работа над меню ответов

--- src/pages/hypothesisOne/HypothesisOne.js
+++ src/pages/hypothesisOne/HypothesisOne.js
@@ -6,6 +6,7 @@ const HyphothesisOne = ({resultFirst, setResult}) => {
     const [rightAnswers, setRightAnswers] = useState([]);
     const [tasksCounter, setTasksCounter] = useState(0);
     const [resultLight, setResultLight] = useState(0);
+    const [resultDark, setResultDark] = useState(0);
 
     const symbols = [587,  431, 861, 902, 213, '✎','✐', '❆', '☾', '♡'];
 
--- src/pages/hypothesisOne/tasks/Tasks.js
+++ src/pages/hypothesisOne/tasks/Tasks.js
@@ -2,52 +2,64 @@ import React, {useState, useEffect} from 'react';
 
 
 const Tasks = ({rightAnswers, 
-        setRightAnswers, 
         symbols,
         tasksCounter,
-        setTasksCounter
+        setTasksCounter,
+        setResultLight,
+
     }) => {
 
-    
+    function handleClick(symbol) {
+        if (rightAnswers.indexOf(symbol) !== -1) {
 
-    function getRandom(min, max) {
-        return Math.floor(Math.random () * (max - min + 1)) + min;
-    };
-
-    function getRandomArray() {
-        const rightAnswersArray = [];
-
-        while (rightAnswersArray.length != 4) {
-            const symbol = symbols[getRandom(0, 9)];
-            if(rightAnswersArray.indexOf(symbol) === -1) {
-                console.log(symbol)
-                rightAnswersArray.push(symbol);
-            }
         }
-
-        return rightAnswersArray;
-    };
-
-    useEffect(() => {
-        setRightAnswers(getRandomArray());
-    }, []);
+    }
     
     return (
-        <div className="test__container">
-            <div className="test">
-                <div className={tasksCounter < 3 ? 'test__item' : 'test__item_dark'}>
-                    {rightAnswers[0]}
+        <div className="tasks__container">
+            <form className="tasks__form">
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[0]} name="item" />
+                    <label for={symbols[0]}>{symbols[0]}</label>
                 </div>
-                <div className={tasksCounter < 3 ? 'test__item' : 'test__item_dark'}>
-                    {rightAnswers[1]}
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[1]} name="item" />
+                    <label for={symbols[1]}>{symbols[1]}</label>
                 </div>
-                <div className={tasksCounter < 3 ? 'test__item' : 'test__item_dark'}>
-                    {rightAnswers[2]}
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[2]} name="item" />
+                    <label for={symbols[2]}>{symbols[2]}</label>
                 </div>
-                <div className={tasksCounter < 3 ? 'test__item' : 'test__item_dark'}>
-                    {rightAnswers[3]}
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[3]} name="item" />
+                    <label for={symbols[3]}>{symbols[3]}</label>
                 </div>
-            </div>
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[4]} name="item" />
+                    <label for={symbols[4]}>{symbols[4]}</label>
+                </div>
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[5]} name="item" />
+                    <label for={symbols[5]}>{symbols[5]}</label>
+                </div>
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[6]} name="item" />
+                    <label for={symbols[6]}>{symbols[6]}</label>
+                </div>
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[7]} name="item" />
+                    <label for={symbols[7]}>{symbols[7]}</label>
+                </div>
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[8]} name="item" />
+                    <label for={symbols[8]}>{symbols[8]}</label>
+                </div>
+                <div className='tasks__item'>
+                    <input type="checkbox" id={symbols[9]} name="item" />
+                    <label for={symbols[9]}>{symbols[9]}</label>
+                </div>
+                
+            </form>
         </div>
     )
 };
